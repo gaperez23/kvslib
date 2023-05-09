a03013c6793c00308a4e5d3f30e88621bbfe1d0c
Gastón Pérez Faliti <gaston.perez@mercadolibre.com>
2023-05-09 20:06:43 -0300

    Test with global secondary index

--- kvs.go
+++ kvs.go
@@ -2,11 +2,8 @@ package kvslib
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
 	"log"
-	"strconv"
-	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -14,6 +11,12 @@ import (
 	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+)
+
+const (
+	primaryKey     = "primary_key"
+	secondaryKey   = "secondary_key"
+	secondaryIndex = "secondary_index"
 )
 
 type KVS struct {
@@ -24,16 +27,20 @@ type KVS struct {
 }
 
 type KVSClient struct {
-	client    *dynamodb.Client
-	tableName string
-}
-
-type kvsItem struct {
-	Key   string `dynamodbav:"key"`
-	Value []byte `dynamodbav:"value"`
+	client         *dynamodb.Client
+	tableName      string
+	secondaryIndex bool
 }
 
 func (kvs KVS) NewClient(name string) *KVSClient {
+	return kvs.newClient(name, false)
+}
+
+func (kvs KVS) NewClientWithSecondaryIndex(name string) *KVSClient {
+	return kvs.newClient(name, true)
+}
+
+func (kvs KVS) newClient(name string, secondaryIndex bool) *KVSClient {
 	if name == "" {
 		return nil
 	}
@@ -55,8 +62,9 @@ func (kvs KVS) NewClient(name string) *KVSClient {
 	}
 
 	return &KVSClient{
-		client:    dynamodb.NewFromConfig(sdkConfig),
-		tableName: name,
+		client:         dynamodb.NewFromConfig(sdkConfig),
+		tableName:      name,
+		secondaryIndex: secondaryIndex,
 	}
 }
 
@@ -67,7 +75,12 @@ func (kvsClient KVSClient) BootStrap() error {
 	}
 	if !exists {
 		log.Printf("creating table %s...\n", kvsClient.tableName)
-		err = kvsClient.createTable()
+
+		if kvsClient.secondaryIndex {
+			err = kvsClient.createTableWithSecondaryIndex()
+		} else {
+			err = kvsClient.createTable()
+		}
 		if err != nil {
 			return err
 		}
@@ -98,79 +111,18 @@ func (kvsClient KVSClient) tableExists() (bool, error) {
 	return exists, err
 }
 
-func (kvsClient KVSClient) createTable() error {
-	_, err := kvsClient.client.CreateTable(context.TODO(), &dynamodb.CreateTableInput{
-		AttributeDefinitions: []types.AttributeDefinition{{
-			AttributeName: aws.String("key"),
-			AttributeType: types.ScalarAttributeTypeS,
-		},
-		},
-		KeySchema: []types.KeySchemaElement{{
-			AttributeName: aws.String("key"),
-			KeyType:       types.KeyTypeHash,
-		}},
-		TableName: aws.String(kvsClient.tableName),
-		ProvisionedThroughput: &types.ProvisionedThroughput{
-			ReadCapacityUnits:  aws.Int64(10),
-			WriteCapacityUnits: aws.Int64(10),
-		},
-	})
-	if err != nil {
-		log.Printf("couldn't create table %s. Here's why: %s\n", kvsClient.tableName, err)
-	} else {
-		waiter := dynamodb.NewTableExistsWaiter(kvsClient.client)
-		err = waiter.Wait(context.TODO(), &dynamodb.DescribeTableInput{
-			TableName: aws.String(kvsClient.tableName)}, 5*time.Minute)
-		if err != nil {
-			log.Printf("wait for table exists failed. Here's why: %s\n", err)
-		}
-	}
-	return err
-}
-
-func (kvsClient KVSClient) PutItem(ctx context.Context, key int64, value interface{}) error {
-	bytes, err := json.Marshal(value)
-	if err != nil {
-		return err
-	}
-
-	item, err := attributevalue.MarshalMap(kvsItem{
-		Key:   strconv.FormatInt(key, 10),
-		Value: bytes,
-	})
-	if err != nil {
-		return err
-	}
-	_, err = kvsClient.client.PutItem(ctx, &dynamodb.PutItemInput{
-		TableName: aws.String(kvsClient.tableName),
-		Item:      item,
-	})
-	if err != nil {
-		log.Printf("Couldn't add item to table. Here's why: %v\n", err)
-	}
-	return err
-}
-
-func (kvsClient KVSClient) GetItem(ctx context.Context, key int64) ([]byte, error) {
-	item := kvsItem{Key: strconv.FormatInt(key, 10)}
-	response, err := kvsClient.client.GetItem(context.TODO(), &dynamodb.GetItemInput{
-		Key: item.getKey(), TableName: aws.String(kvsClient.tableName),
-	})
-	if err != nil {
-		log.Printf("couldn't get info about %d. Here's why: %s\n", key, err)
-	} else {
-		err = attributevalue.UnmarshalMap(response.Item, &item)
-		if err != nil {
-			log.Printf("couldn't unmarshal response. Here's why: %v\n", err)
-		}
-	}
-	return item.Value, err
-}
-
-func (item kvsItem) getKey() map[string]types.AttributeValue {
-	key, err := attributevalue.Marshal(item.Key)
+func (item kvsItem) getPrimaryKey() map[string]types.AttributeValue {
+	key, err := attributevalue.Marshal(item.PrimaryKey)
 	if err != nil {
 		panic(err)
 	}
-	return map[string]types.AttributeValue{"key": key}
+	return map[string]types.AttributeValue{primaryKey: key}
 }
+
+// func (item kvsItem) getSecondaryKey() map[string]types.AttributeValue {
+// 	key, err := attributevalue.Marshal(*item.SecondaryKey)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	return map[string]types.AttributeValue{secondaryKey: key}
+// }
